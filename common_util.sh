680de158ef65a681dc43af5fabb1afd3035a14e9
ilefter <InLefter@users.noreply.github.com>
2021-12-27 19:36:21 +0800

    add tar grep func

--- common_util.sh
+++ common_util.sh
@@ -10,3 +10,15 @@ function call_func()
 }
 
 cat *.tar.gz | tar -zxf - -i --to-command='grep -Hn --label="$TAR_ARCHIVE/$TAR_FILENAME" -E "regex_pattern" || true'
+
+
+function tgrep()
+{
+    export PATTERN="${1?}"
+    shift
+    for file do
+      tar --to-command='
+        grep -aPH --label="$TAR_ARCHIVE[$TAR_FILENAME]" -e "$PATTERN" || true
+      ' -xf "$file"
+    done
+}

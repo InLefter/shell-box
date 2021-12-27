5d3a28ad52006cadc40b78f65c8992cceb8218e7
ilefter <InLefter@users.noreply.github.com>
2021-12-27 19:23:26 +0800

    grep tar file

--- common_util.sh
+++ common_util.sh
@@ -8,3 +8,5 @@ function call_func()
     echo "<===${1}:[ret:${ret}]"
     return $ret
 }
+
+cat *.tar.gz | tar -zxf - -i --to-command='grep -Hn --label="$TAR_ARCHIVE/$TAR_FILENAME" -E "regex_pattern" || true'
